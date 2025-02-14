9ab8dfcd60aa46447fa7ad7dd640ea74c1e6dfc8
Студент <student@L304-11.vkipo.local>
2025-02-14 10:20:20 +0300

    homework 2

--- 3_sem.py
+++ 3_sem.py
@@ -47,3 +47,16 @@
 #     print("да")
 # else: 
 #     print("нет")
+
+# №2
+# ___
+
+# x, y =int(input('введите X: ')), int(input('введите Y: '))
+# if x>0 and y>0:
+#     print(1)
+# elif x<0 and y>0:
+#     print(2)
+# elif x<0 and y<0:
+#     print(3)
+# else: 
+#     print(4)
